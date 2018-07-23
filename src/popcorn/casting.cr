a6bb64abd261a715d66174e9f1bfd124b661b8ea
icyleaf <icyleaf.cn@gmail.com>
2018-07-23 14:13:39 +0800

    doc: update comments

--- src/popcorn/casting.cr
+++ src/popcorn/casting.cr
@@ -3,6 +3,7 @@ require "yaml/any"
 
 module Popcorn
   module Casting
+    # Returns the `Int32` value represented by given data type.
     def to_int(v : T) forall T
       case v
       when Bool
@@ -21,18 +22,24 @@ module Popcorn
       end
     end
 
+    # Returns the `Int64` value represented by given data type.
     def to_int64(v : T) forall T
     end
 
+    # Returns the `Float64` value represented by given data type.
     def to_float(v : T) forall T
     end
 
+    # Returns the `Float32` value represented by given data type.
     def to_float32(v : T) forall T
     end
 
+    # Returns the `Time` value represented by given data type.
     def to_time(v : T) forall T
     end
 
+    # Returns the `Bool` value represented by given data type.
+    # It accepts true, t, yes, y, on, 1, false, f, no, n, off, 0. Any other value return an error.
     def to_bool(v : T) forall T
       value = case v
       when Bool
@@ -67,6 +74,7 @@ module Popcorn
 
     {% for method in @type.methods %}
       def {{ method.name.id }}?(v : T) forall T
+        # Returns the `{{ method.name }}` value represented by given data type. If can not convert return nil.
         {{ method.name.id }}(v)
       rescue TypeCastError
         nil
